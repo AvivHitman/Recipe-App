6f94d49a0b3686bc8e5b89d74df17786cb8b7a22
aviv tfilin <avivt193@gmail.com>
2021-06-24 14:22:13 +0300

    Create FoodData.java

---
+++ app/src/main/java/com/example/recipeapp/FoodData.java
@@ -0,0 +1,52 @@
+package com.example.recipeapp;
+
+import java.util.ArrayList;
+
+public class FoodData {
+
+    private String itemName, itemImage, key;
+    private ArrayList<String> description, ingredient;
+    private int timer;
+
+    public FoodData() {
+    }
+    public FoodData(String itemName, String itemImage,ArrayList<String> ingredients,ArrayList<String> description, int timer) {
+        this.itemName = itemName;
+        this.itemImage = itemImage;
+        this.ingredient = ingredients;
+        this.description = description;
+        this.timer = timer;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public ArrayList<String> getDescription() {
+        return description;
+    }
+
+    public ArrayList<String> getIngredient() {
+        return ingredient;
+    }
+
+    public String getItemImage() {
+        return itemImage;
+    }
+
+    public String getKey() { return key; }
+
+    public int getTimer() { return timer; }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setDescription(ArrayList<String> description) {
+        this.description = description;
+    }
+
+    public void setIngredient(ArrayList<String> ingredient) {
+        this.ingredient = ingredient;
+    }
+}
